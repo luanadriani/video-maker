4104b652d9b61fd9513a4efc76164f2532f22911
Luan Adriani <luanadriani2011@gmail.com>
2019-12-10 02:01:15 +0000

    feat: narration robot and get duration of narrarion

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const robots = {
 	input: require('./robots/input.js'),
 	text: require('./robots/text.js'),
 	state: require('./robots/state.js'),
-	image: require('./robots/image.js'),
+    image: require('./robots/image.js'),
+    narration: require('./robots/narration.js'),
 	video: require('./robots/video.js'),
 	youtube: require('./robots/youtube.js')
 }
 
 async function start(){
-	robots.input()
-	await robots.text()
-	await robots.image()
-	await robots.video()
-	await robots.youtube()
+	//robots.input()
+    //await robots.text()
+    await robots.narration()
+	//await robots.image()
+	//await robots.video()
+	//await robots.youtube()
 }
 
 start()
